638d8b3e5dc6b16686a6fa7d922fc5a65ca68d3c
silver_kir <rci@mp03.ru>
2023-10-09 21:12:59 +0800

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+using System.Reflection.Emit;
+
+Console.Write("Введите число:  ");
+int userNumber1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine (Math.Abs((userNumber1/10)%10));
