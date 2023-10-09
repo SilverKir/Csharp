cae181d9de59c9e039c9afac2909b2de3f8f35ee
silver_kir <rci@mp03.ru>
2023-10-09 21:28:51 +0800

    Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,9 +5,9 @@
 
 Console.Write("Введите число:  ");
 int userNumber1=Convert.ToInt32(Console.ReadLine());
-if (userNumber1/100 == 0) {
-    Console.WriteLine("третьей цифры нет");
-}
-else{
-Console.WriteLine (Math.Abs((userNumber1/100)%10));
-}
+    if (userNumber1/100 == 0) {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else{
+    Console.WriteLine (Math.Abs((userNumber1/100)%10));
+    }
