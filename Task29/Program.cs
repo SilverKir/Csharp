fbf099285c71d51122582d9dcfc40350075e52e5
silver_kir <rci@mp03.ru>
2023-10-16 21:52:13 +0800

    Task29 done

---
+++ Task29/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int Rrompt(string massage)
+{
+    System.Console.Write(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] AddArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = Rrompt($"Введите значение {i} элемента массива: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write("[");
+    while (position < count)
+    {
+        if (position == (count - 1))
+        {
+            Console.Write($"{col[position]}");
+            position++;
+        }
+        else
+        {
+            Console.Write($"{col[position]},");
+            position++;
+        }
+    }
+    Console.Write("]");
+}
+
+Console.Clear();
+int temp = Rrompt("Введите длину массива: ");
+int[] result = AddArray(temp);
+Console.Write($"Ваш массив: ");
+PrintArray(result);
