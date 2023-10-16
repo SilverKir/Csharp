29f73a9f1d4a02924741c11469dc3d04655604bd
silver_kir <rci@mp03.ru>
2023-10-16 14:33:32 +0800

    Task24 Summ

---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+int Rrompt (string  massage)
+{
+    System.Console.Write(massage);
+    string value = Console.ReadLine();
+    int result= Convert.ToInt32(value);
+    return result;
+}
+
+int SummNumber(int A) {
+    int result = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+Console.WriteLine(SummNumber(Rrompt ("Введите число: ")));
